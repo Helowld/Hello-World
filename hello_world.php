ee4eb53126fcd54e9b5590f1ddd1bf8719d6161f
Zentreaxᴰᵉᵛ <36458019+Zentreax@users.noreply.github.com>
2020-10-04 00:25:47 +0200

    Zentreax; hello_world.php; Added hello world code.

---
+++ hello_world.php
@@ -0,0 +1,8 @@
+<?php
+    print("hello world");
+    // Not much to explain here, this article does a great job:
+    // https://phphelloworld.com/
+
+    // ~$ php hello_world.php
+    // Output: hello world
+?>
