49e8bbc960c434107b2a515ac16a58b57bfb3e99
jehunseo <benbot591@gmail.com>
2020-10-04 22:30:53 +0900

    "add comments for hello_world.kt"

--- hello_world.kt
+++ hello_world.kt
@@ -1,3 +1,10 @@
+//this excution of kotlin starts from the main() function.
 fun main(args:Array<String>){
+    //println() displays a string inside quotation and newline('\n') command
     println("Hello world!")
 }
+
+//for more info, check this : https://www.programiz.com/kotlin-programming/hello-world
+
+//OUTPUT:
+//Hello world!
