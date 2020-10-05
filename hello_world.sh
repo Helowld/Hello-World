7f918bb2a91b2497697bbddabdb221ac378a6488
Tobias Schröder <5970416+tbsschroeder@users.noreply.github.com>
2020-10-05 08:08:26 +0200

    Create hello_world.sh
    Add hello world in bash as well as description to run

---
+++ hello_world.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Never underestimate the power of simple and small bash scripts
+# How to run this bash file:
+#   Windows     bash hello_world.sh
+#   Mac/Linux   ./hello_world.sh
+# It may occur that the right for an execution are missing, then please:
+#   chmod +x hello_world.sh
+
+# This program will put "Hello World" on your terminal.
+echo "Hello World"
