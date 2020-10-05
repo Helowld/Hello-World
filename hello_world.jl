d419c3d6ee63658a529b0f374786a3a12fc3b4e5
Christopher Greening <36808060+chris-greening@users.noreply.github.com>
2020-10-05 01:36:57 -0400

    Updated with additional information

--- hello_world.jl
+++ hello_world.jl
@@ -1,6 +1,13 @@
 #Official site: https://julialang.org/
 
-#Simple syntax, here's some more info: https://juliabyexample.helpmanual.io/
+#Simple syntax, no need to include any libraries for terminal output
 println("Hello world!")
 
+#From Julia REPL
+#julia> include("path/to/hello_world.jl")
+
+#From terminal 
+#$ /path/to/julia hello_world.jl
+
 #Output: Hello world!
+#Here's some more info/tutorials: https://juliabyexample.helpmanual.io/
