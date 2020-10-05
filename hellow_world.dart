ce500fabfa3a0a8d5165eb3a854c1b4b7394cc04
Tobias Schröder <5970416+tbsschroeder@users.noreply.github.com>
2020-10-05 08:03:10 +0200

    Update and rename hellow_world.dart to hello_world.dart
    Add additional description and fix filename

--- hellow_world.dart
+++
@@ -1,8 +0,0 @@
-// Official website: https://dart.dev/samples
-
-// Executes "hello world" in Dart, which is
-// heaviliy used for cross app dev in Flutter
-
-void main() {
-  print('Hello, World!');
-}
