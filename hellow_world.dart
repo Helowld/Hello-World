a7e99ff7b0d77007aaaa82a329d2008fc2caf853
Tobias Schröder <5970416+tbsschroeder@users.noreply.github.com>
2020-10-05 07:21:14 +0200

    Create hellow_world.dart
    Add hello world in Dart

---
+++ hellow_world.dart
@@ -0,0 +1,8 @@
+// Official website: https://dart.dev/samples
+
+// Executes "hello world" in Dart, which is
+// heaviliy used for cross app dev in Flutter
+
+void main() {
+  print('Hello, World!');
+}
