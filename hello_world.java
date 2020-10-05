88abb874fc1d3edf5da50736987ea6b06b335bc6
hms5232 <43672033+hms5232@users.noreply.github.com>
2020-10-05 16:59:40 +0800

    Create hello_world.java

---
+++ hello_world.java
@@ -0,0 +1,22 @@
+/*
+ * Oracle Java official site: https://www.java.com/zh-TW/
+ * OpenJDK official site: https://openjdk.java.net/
+ */
+
+// Java Class name should start at capital
+public class HelloWorld{
+	/*
+	This is a comment
+	*/
+	// This is a comment too
+	// main function
+	public static void main(String []args){
+		System.out.println("Hello World");  // show Hello World on terminal (with newline)
+		System.out.print("Hello World");  // show Hello World on terminal
+		System.out.print("!");  // show ! on terminal
+	}
+}
+
+// output:
+// Hello world
+// Hello World!
