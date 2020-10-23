7368e08b8b428784c67bc30f71a4dbaefc247a7a
hieetesh <dharmadhikarihitesh@gmail.com>
2020-10-23 15:02:17 +0530

    Added V Language

---
+++ hello.v
@@ -0,0 +1,14 @@
+/*
+*   V Lang Official Website: https://vlang.io/
+*   V Lang documentation: https://github.com/vlang/v/blob/master/doc/docs.md
+*/
+
+
+fn main() {
+    println('Hello World')
+}
+
+/*
+Command to execute: v run hello.v
+Output: Hello World
+*/
