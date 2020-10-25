c134555067046b47ff0de23bc5695ce51784556e
Sha-Kumar <60093320+Sha-Kumar@users.noreply.github.com>
2020-10-25 21:08:44 +0530

    Create hello_world.swift

---
+++ hello_world.swift
@@ -0,0 +1,15 @@
+// Hello, World! Program
+
+// 'import' keyword allows you to access all the symbols defined inside a framework.
+import Swift
+
+// The below line is called a function in Swift. More specifically, a print function.
+// In swift, print means "show on the screen". The above code prints the string inside quotation marks, i.e "Hello, World!" on the screen.
+print("Hello, World!")
+
+
+// For more info check this official website https://swift.org/
+// For more info check this https://www.programiz.com/swift-programming
+
+// OUTPUT :
+// Hello, World!
