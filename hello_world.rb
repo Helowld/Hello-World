3d9b53652116ee8acd492f7e95c58cb3621f770c
Sha-Kumar <60093320+Sha-Kumar@users.noreply.github.com>
2020-11-01 19:50:54 +0530

    Update hello_world.rb

--- hello_world.rb
+++ hello_world.rb
@@ -1,14 +1,9 @@
 # The `puts` is a Ruby method that tells the computer to print some text to the screen.
-# The `puts` method is then followed by a sequence of characters — `Hello, World!`, enclosed in quotation marks.
-# The `puts` method will print this string to the screen when the program runs.
+# Methods, like the `puts` method, are included in Ruby by default. 
 puts "Hello, World!"
 
-# Some methods, like the `puts` method, are included in Ruby by default. 
-# These built-in methods are always available when you create Ruby programs. We can also define your own methods.
-
-
 # For more info check this microsoft's official website https://www.ruby-lang.org/en/
-# For more info check this https://www.digitalocean.com/community/tutorial_series/how-to-code-in-ruby
+# For references to that check the site https://www.digitalocean.com/community/tutorial_series/how-to-code-in-ruby
 # For refernces to that check the site https://www.geeksforgeeks.org/ruby-programming-language/
 
 
